26e088cf25068a5f9a09bc1959f8517078bf2d2c
robinrob <robin@mrrobinsmith.com>
2015-06-27 17:53:45 +0100

    Quick-save.

--- gulpfile.js
+++ gulpfile.js
@@ -161,7 +161,7 @@ gulp.task('purge-online-cache', function() {
 });
 
 gulp.task('save', function(done) {
-    return require('child_process', done).exec('git add . && git commit -m Auto-update && git push origin develop', {
+    return require('child_process', done).exec('rake base: save && git push origin develop', {
         stdio: 'inherit'
     }, done);
 });
