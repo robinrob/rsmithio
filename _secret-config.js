8240c84bec0db92c06d656903ea525ec21cca564
robinrob <robin@mrrobinsmith.com>
2015-06-27 23:05:26 +0100

    Quick-save.

--- _secret-config.js
+++ _secret-config.js
@@ -4,6 +4,8 @@ exports = module.exports = function(config) {
             token  : '431609236d8480cf29f6cfbc2cbe34374ced5',
             email  : 'rs@rsmith.io',
             domain : 'rsmith.io'
-        }
+        },
+        siteDomain: "rsmith.io",
+        sitemapUrl: "https://rsmith.io/sitemap.xml"
     })
 }
