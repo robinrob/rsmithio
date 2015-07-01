75df378324b82b70b0e8029f2530350baea19b0a
robinrob <robin@mrrobinsmith.com>
2015-07-01 17:16:44 +0100

    Contact failure msg

--- _js/7contact.js
+++ _js/7contact.js
@@ -59,7 +59,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later or drop me an email at: robin@rsmith.io");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
