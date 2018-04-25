c04250c40daf5955e823d447f148cf8a8645e8d1
Robin Smith <robin@rsmith.io>
2018-04-25 21:44:03 +0100

    Quick-save.

--- gulpfile.js
+++ gulpfile.js
@@ -281,9 +281,7 @@ gulp.task('dev-build', gulp.series('haml-build', 'jekyll', 'sass', gulp.parallel
 gulp.task('fast-dev-build', gulp.series('jekyll', 'sass', gulp.parallel('css-dev', 'js'), 'reload'))
 
 // Deploy
-gulp.task('fast-deploy', gulp.series('sitemap', 'submit-sitemap'))
-
-gulp.task('deploy', gulp.series('build', 'fast-deploy'))
+gulp.task('deploy', gulp.series('build', 'sitemap', 'submit-sitemap'))
 
 // Build/watch
 gulp.task('full', gulp.series('build', gulp.parallel('haml-watch', 'watch', 'browser-sync')))
